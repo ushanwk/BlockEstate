bbe5dcef1612c00c358cb32db08bc5ba4eab17cf
UshanKaushalya <Ushanwk22@gmail.com>
2025-03-05 01:03:10 +0530

    Routes defined

--- Front_End/src/routes/Index.jsx
+++ Front_End/src/routes/Index.jsx
@@ -2,19 +2,24 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {WebsiteLayout} from "../layouts/WebsiteLayout.jsx";
 import {HomePage} from "../pages/website/HomePage.jsx";
 import {AboutPage} from "../pages/website/AboutPage.jsx";
+import {AuthLayout} from "../layouts/AuthLayout.jsx";
+import {LoginPage} from "../pages/auth/LoginPage.jsx";
 
 export const Index = () => {
     return (
         <BrowserRouter>
-
             <Routes>
+                {/* Website Routes */}
                 <Route path="/" element={<WebsiteLayout />}>
                     <Route index element={<HomePage />} />
                     <Route path="about" element={<AboutPage />} />
                 </Route>
 
+                {/* Auth Routes */}
+                <Route path="/auth" element={<AuthLayout />}>
+                    <Route index element={<LoginPage />} />
+                </Route>
             </Routes>
-
         </BrowserRouter>
     )
 }
