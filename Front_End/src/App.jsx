daf8fa4e445fa211b53bcbb9653b38b5de887908
UshanKaushalya <Ushanwk22@gmail.com>
2025-03-04 19:10:21 +0530

    Initial commit - React & Tailwind setup

---
+++ Front_End/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <main>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+      </main>
+  )
+}
+
+export default App
